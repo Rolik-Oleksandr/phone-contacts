8998b34cc844b00ddf745540d3a5f219c8f2792d
richn <rolik.alexx@gmail.com>
2023-07-06 17:23:14 +0300

    implement User detail service

---
+++ src/main/java/phone/contacts/rest/api/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package phone.contacts.rest.api.service.impl;
+
+import static org.springframework.security.core.userdetails.User.UserBuilder;
+import static org.springframework.security.core.userdetails.User.withUsername;
+
+import phone.contacts.rest.api.model.User;
+
+import java.util.Optional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import phone.contacts.rest.api.service.UserService;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userService.findByEmail(email);
+        if (userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("Can't find user by email: " + email);
+        }
+        User user = userOptional.get();
+        UserBuilder builder = withUsername(email);
+        builder.password(user.getPassword());
+        builder.roles(user.getRoles().stream()
+                .map(r -> r.getRoleName().name())
+                .toArray(String[]::new));
+        return builder.build();
+    }
+}
