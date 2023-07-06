59f99f531762a4694028d64647671dfab2b23322
richn <rolik.alexx@gmail.com>
2023-07-06 17:14:15 +0300

    implement UserController

---
+++ src/main/java/phone/contacts/rest/api/controller/UserController.java
@@ -0,0 +1,30 @@
+package phone.contacts.rest.api.controller;
+
+import phone.contacts.rest.api.model.User;
+import phone.contacts.rest.api.model.dto.response.UserResponseDto;
+import phone.contacts.rest.api.service.UserService;
+import phone.contacts.rest.api.service.mapper.ResponseDtoMapper;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final ResponseDtoMapper<UserResponseDto, User> userResponseDtoMapper;
+
+    public UserController(UserService userService,
+                          ResponseDtoMapper<UserResponseDto, User> userResponseDtoMapper) {
+        this.userService = userService;
+        this.userResponseDtoMapper = userResponseDtoMapper;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto findByEmail(@RequestParam String email) {
+        User user = userService.findByEmail(email).orElseThrow(
+                () -> new RuntimeException("User with email " + email + " not found"));
+        return userResponseDtoMapper.mapToDto(user);
+    }
+}
